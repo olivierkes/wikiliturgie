b4bf0726ab954356d83d721beb2b42f98051fc3b
Olivier Keshavjee <olivier.keshavjee@gmail.com>
2018-06-19 15:34:58 +0200

    First tests

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,101 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<v-app>
+  <v-navigation-drawer clipped
+                       v-model="leftDrawer"
+                       app
+                       v-if="$vuetify.breakpoint.smAndDown">
+    <v-list>
+      <v-list-tile value="true"
+                   v-for="(item, i) in items"
+                   :key="i">
+        <v-list-tile-action>
+          <v-icon v-html="item.icon"></v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title v-text="item.title"></v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+  <v-navigation-drawer right
+                       v-model="rightDrawer"
+                       app
+                       clipped
+                       v-if="$vuetify.breakpoint.smAndDown">
+    <v-list>
+      <v-list-tile>
+        <v-list-tile-action>
+          <v-icon>compare_arrows</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-title>Liturgie</v-list-tile-title>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+  <v-toolbar app
+             clipped-left
+             clipped-right
+             class="primary"
+             dark
+             flat
+             dense>
+    <v-toolbar-side-icon @click.stop="leftDrawer = !leftDrawer"></v-toolbar-side-icon>
+    <v-toolbar-title v-text="title"></v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn icon
+           @click.stop="rightDrawer = !rightDrawer">
+      <v-icon>menu</v-icon>
+    </v-btn>
+  </v-toolbar>
+  <v-content>
+    <v-container fluid
+                 fill-height
+                 grid-list-md>
+      <v-layout row
+                wrap>
+        <v-flex xs12
+                md3
+                v-if="$vuetify.breakpoint.mdAndUp">
+          <v-card v-if="leftDrawer">
+            <v-card-title>Comme ça</v-card-title>
+          </v-card>
+        </v-flex>
+        <v-flex xs12
+                md6>
+          <SearchBar/> </v-flex>
+        <v-flex xs12
+                md3
+                v-if="$vuetify.breakpoint.mdAndUp">
+          <v-card v-if="rightDrawer">
+            <v-card-title>Comme ça</v-card-title>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
+  <v-footer :fixed="fixed"
+            app> <span>&copy; 2017</span> </v-footer>
+</v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import SearchBar from './components/SearchBar'
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
+    SearchBar
+  },
+  data() {
+    return {
+      clipped: false,
+      leftDrawer: false,
+      fixed: false,
+      items: [{
+        icon: 'filters',
+        title: 'Filters'
+      }],
+      rightDrawer: false,
+      title: 'WikiLiturgie'
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,87 @@
+<template>
+<div style="height: 100%;">
+  <v-layout column>
+    <v-flex xs12
+            v-show="searchText">
+      <v-toolbar dense>
+        <v-text-field prepend-icon="search"
+                      full-width
+                      hide-details
+                      single-line
+                      v-model="searchText"
+                      ref="searchTop"
+                      @change="focusText"></v-text-field>
+        <v-toolbar-items>
+          <v-btn flat
+                 small>
+            <v-icon>filter</v-icon>
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+    </v-flex>
+    <v-layout align-center
+              justify-center>
+      <v-flex class="text-xs-center"
+              v-show="!searchText"
+              sm10
+              md12>
+        <v-text-field solo
+                      class="mb-2"
+                      v-model="searchText"
+                      ref="searchCenter"
+                      @change="focusText"></v-text-field>
+        <v-btn class="mb-2">
+          <v-icon left>search</v-icon> Rechercher</v-btn>
+        <p class="body-1"> Parmis {{ nItems }} textes liturgiques. </p>
+      </v-flex>
+    </v-layout>
+  </v-layout>
+</div>
+</template>
+      <!-- <v-card > -->      <!-- <v-card > -->      <!-- <v-card > -->      <!-- <v-card > -->
+<script>
+export default {
+  name: 'SearchBar',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      nItems: 24982,
+      searchText: null
+    }
+  },
+  methods: {
+    focusText (){
+      if (this.searchText){
+        // document.getElementById("mytext").focus();
+        this.$refs.searchTop.focus()
+      }
+      else {
+        this.$refs.searchCenter.focus()
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
