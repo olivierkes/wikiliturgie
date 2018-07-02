eedc4ab029d2f12764d37dbb810673f7f51321a5
Olivier Keshavjee <olivier.keshavjee@gmail.com>
2018-07-02 20:45:17 +0200

    Adds: changing user's displayName

--- src/components/User/Profile.vue
+++ src/components/User/Profile.vue
@@ -1,5 +1,5 @@
 <template>
-<v-container grid-list-md>
+<v-container grid-list-lg>
   <v-layout column>
     <v-flex xs12>
       <v-layout row>
@@ -16,18 +16,37 @@
           <v-flex xs12
                   sm6>
             <v-card>
+              <v-toolbar dense
+                         flat
+                         color="grey darken-1"
+                         dark>
+                <v-toolbar-title>Informations</v-toolbar-title>
+              </v-toolbar>
               <v-card-text>
-                <h3>Info</h3>
-                <v-text-field label="Nom affiché"
-                              v-model="userDisplayName"></v-text-field>
+                <v-text-field label="Nom d'auteur"
+                              v-model="userDisplayName"
+                              hint="Le nom d'auteur affiché pour cet utilisateur. Prénom + Nom."></v-text-field>
               </v-card-text>
+              <v-card-actions>
+                <v-btn flat
+                       color="amber darken-4"
+                       @click="saveDisplayName">Enregistrer</v-btn>
+                <v-btn flat
+                       color="grey"
+                       @click="displayName=user.displayName">Annuler</v-btn>
+              </v-card-actions>
             </v-card>
           </v-flex>
           <v-flex xs12
                   sm6>
             <v-card>
+              <v-toolbar dense
+                         flat
+                         color="grey darken-1"
+                         dark>
+                <v-toolbar-title>Debug</v-toolbar-title>
+              </v-toolbar>
               <v-card-text>
-                <h3>Debug</h3>
                 <p> User: {{ user}} </p>
               </v-card-text>
             </v-card>
@@ -40,18 +59,30 @@
 </template>
 
 <script>
+import {snackbar} from "@/utils"
+import firebase from "firebase"
+import Vuex from "vuex"
+import { db } from "@/firebase"
 export default {
-  data () {
+  data() {
     return {
-      displayName: ""
+      displayName: "",
+      loaded: false
     }
   },
-  computed: {
-    user() {
-      return this.$store.getters["users/user"]
-    },
-    userDisplayName() {
-      get: this.user.displayName
+  computed: {...Vuex.mapGetters({
+      authors: "authors/authors",
+      user: "users/user"
+    }),
+    userDisplayName: {
+      get: function () {
+        if (!this.loaded) {
+          this.displayName = this.user.displayName
+          this.loaded = true
+        }
+        return this.displayName
+      },
+      set: function (val) { this.displayName = val }
     }
   },
   watch: {
@@ -64,6 +95,21 @@ export default {
   methods: {
     signOut() {
       this.$store.dispatch("users/signOut")
+    },
+    saveDisplayName() {
+      if (this.displayName) {
+        this.user.updateProfile({
+          displayName: this.displayName
+        }).then(() => snackbar("Bonjour " + this.displayName+". Quel joli nom!"))
+
+        // If user is a author, we need to update that too
+        var author = this.authors.find(a => a.user == this.user.uid)
+        if (author) {
+          db.collection("authors").doc(author.id).update({name: this.displayName})
+        }
+      } else {
+        snackbar("Le nom ne peut pas être vide.")
+      }
     }
   }
 }
