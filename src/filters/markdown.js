ab266c6d2dee2e30decc546ad0883f87aa544457
Olivier Keshavjee <olivier.keshavjee@gmail.com>
2018-07-15 00:03:55 +0200

    Uses `marked` instead of `markdown`. Same as SimpleMDE, and preserves line breaks (GFM)

--- src/filters/markdown.js
+++ src/filters/markdown.js
@@ -1,4 +1,12 @@
-var markdown = require("markdown").markdown
+// var markdown = require("markdown").markdown
+
+import marked from 'marked'
+marked.setOptions({
+  gfm: true,
+  breaks: true
+})
+
 export default (mdContent) => {
-  return markdown.toHTML(mdContent, "Maruku")
+  // return markdown.toHTML(mdContent, "Maruku")
+  return marked(mdContent)
 }
